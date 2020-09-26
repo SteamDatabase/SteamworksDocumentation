8f050f2812a43af3cba99dbb036ac2952ed7df27
Pavel Djundik <xPaw@users.noreply.github.com>
2020-09-26 19:45:23 +0300

    Better commit message

--- update.sh
+++ update.sh
@@ -6,5 +6,5 @@ php crawl.php
 yarn format-docs
 
 git add -A
-git commit -S -a -m "Update documentation"
+git commit -S -a -m "$(git status --porcelain | wc -l) files | $(git status --porcelain|awk '{print "basename " $2}'| sh | sed '{:q;N;s/\n/, /g;t q}')"
 git push
