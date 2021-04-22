3e7e4cd0735f2a82a0cbf79502ac035d9b72afdb
Pavel Djundik <xPaw@users.noreply.github.com>
2021-04-22 10:53:44 +0300

    Switch to npm

--- update.sh
+++ update.sh
@@ -2,9 +2,9 @@
 
 cd "$(dirname "$0")"
 
-yarn
+npm install
 php crawl.php
-yarn format-docs
+npm run format-docs
 
 git add -A
 git commit -a -m "$(git status --porcelain | wc -l) files | $(git status --porcelain|awk '{print "basename " $2}'| sh | sed '{:q;N;s/\n/, /g;t q}')" || exit 0
